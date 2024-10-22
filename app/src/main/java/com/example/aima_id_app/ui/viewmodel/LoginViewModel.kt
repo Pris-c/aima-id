237aee1468d6d1866ec49418e35c4a9f9924bf7c
Priscila <priscila.campoos@yahoo.com.br>
2024-10-22 12:04:53 +0100

    document LoginViewModel

--- app/src/main/java/com/example/aima_id_app/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/aima_id_app/ui/viewmodel/LoginViewModel.kt
@@ -6,6 +6,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.aima_id_app.data.repository.AuthRepository
 
+/**
+ * ViewModel for handling the login functionality in the application.
+ *
+ * This ViewModel interacts with the AuthRepository to manage user authentication,
+ * and exposes LiveData objects to observe the login status and navigation events.
+ */
 class LoginViewModel : ViewModel() {
 
     private val authRepository = AuthRepository()
@@ -16,8 +22,16 @@ class LoginViewModel : ViewModel() {
     private val _navigateToActivity = MutableLiveData<Class<*>>()
     val navigateToActivity: LiveData<Class<*>> get() = _navigateToActivity
 
-//---------------------------------------------------------------------------------
 
+    /**
+     * Attempts to log in the user with the provided email and password.
+     *
+     * @param email The email address of the user.
+     * @param password The password for the user.
+     *
+     * If the login is successful, it updates the navigation LiveData to direct the user
+     * to the UserActivity. If the login fails, it sets an error message in the LiveData.
+     */
     fun login(email: String, password: String) {
         authRepository.login(email, password) { userId ->
             if (userId != null){
