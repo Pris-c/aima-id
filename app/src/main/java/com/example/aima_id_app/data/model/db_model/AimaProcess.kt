6e8e03380836fb0517d77373f02527dd94367f89
pris-c <priscila.campoos@yahoo.com.br>
2024-10-31 10:22:11 +0000

    document AimaProcess

--- app/src/main/java/com/example/aima_id_app/data/model/db_model/AimaProcess.kt
+++ app/src/main/java/com/example/aima_id_app/data/model/db_model/AimaProcess.kt
@@ -3,6 +3,17 @@ package com.example.aima_id_app.data.model.db_model
 import com.example.aima_id_app.util.enums.ProcessStatus
 import java.time.LocalDate
 
+/**
+ * Data class representing an Aima process.
+ *
+ * @property userId The ID of the user associated with the process.
+ * @property serviceCode The code of the service related to the process.
+ * @property status The current status of the process (default is IN_ANALYSIS).
+ * @property createdAt The date the process was created (default is the current date).
+ * @property updatedAt The date the process was last updated (default is the current date).
+ *
+ * This class holds information about a specific process in the Aima system.
+ */
 data class AimaProcess (
     val userId: String = "",
     val serviceCode: String = "",
