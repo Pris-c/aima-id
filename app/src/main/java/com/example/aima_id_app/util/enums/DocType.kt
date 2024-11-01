d3d507a5322cb13cce692b87066b575da0e5b5c4
Priscila <99100607+Pris-c@users.noreply.github.com>
2024-11-01 11:36:00 +0000

    update enums with presentation strings (#28)

--- app/src/main/java/com/example/aima_id_app/util/enums/DocType.kt
+++ app/src/main/java/com/example/aima_id_app/util/enums/DocType.kt
@@ -5,6 +5,17 @@ package com.example.aima_id_app.util.enums
  * Enum class representing different types of documents.
  * These document types can be used to categorize or identify various documents a user may possess.
  */
-enum class DocType {
-    PASSPORT, RESIDENCE_VISA, VISA, RESIDENT_PERMIT, ADDRESS_PROOF, EMPLOYMENT_CONTRACT, CRIMINAL_RECORD, SCHOOL_REGISTRATION, HEALTH_INSURANCE, NIF, NISS, CITIZENSHIP_ID, PROOF_INCOME
+enum class DocType(doc: String) {
+    PASSPORT("Passaporte"),
+    VISA("Visto"),
+    RESIDENT_PERMIT("Autorização de Residência"),
+    ADDRESS_PROOF("Comprovativo de morada"),
+    EMPLOYMENT_CONTRACT("Contrato de Trabalho"),
+    CRIMINAL_RECORD("Registro Criminal"),
+    SCHOOL_REGISTRATION("Matricula Escolar"),
+    HEALTH_INSURANCE("Seguro Saúde"),
+    NIF("NIF"),
+    NISS("NISS"),
+    CITIZENSHIP_ID("Atestado de Nacionalidade"),
+    PROOF_INCOME("Comprovativo de Meios de Subsistência")
 }
