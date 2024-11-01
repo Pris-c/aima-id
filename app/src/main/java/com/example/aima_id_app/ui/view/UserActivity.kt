97b59a867fa5b891c5fdbb96294fe4a902fb223c
Gabriel Beli <gabrielimabelo@gmail.com>
2024-11-01 03:19:48 +0000

    service user

--- app/src/main/java/com/example/aima_id_app/ui/view/UserActivity.kt
+++ app/src/main/java/com/example/aima_id_app/ui/view/UserActivity.kt
@@ -7,9 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.example.aima_id_app.R
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,42 +16,7 @@ class UserActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user)
 
         findViewById<View>(R.id.card_service).setOnClickListener {
-            loadFragment(RequestServiceFragment())
-        }
-
-        findViewById<View>(R.id.card_upload).setOnClickListener {
-            loadFragment(UploadDocsFragment())
-        }
-
-        findViewById<View>(R.id.card_scheduling).setOnClickListener {
-            loadFragment(SchedulingFragment())
-        }
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> {
-                    supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
-                    true
-                }
-                R.id.nav_profile -> {
-                    loadFragment(ProfileFragment())
-                    true
-                }
-                R.id.nav_calendar -> {
-                    loadFragment(SchedulingUserFragment())
-                    true
-                }
-                R.id.nav_notifications -> {
-                    loadFragment(SchedulingFragment())
-                    true
-                }
-                R.id.nav_contact -> {
-                    loadFragment(SchedulingFragment())
-                    true
-                }
-                else -> false
-            }
+            loadFragment(RequestResidencyFragment())
         }
 
 
@@ -74,21 +37,15 @@ class UserActivity : AppCompatActivity() {
      * @param fragment The fragment to be loaded.
      */
     private fun loadFragment(fragment: Fragment) {
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-
-        if (currentFragment == null || currentFragment::class != fragment::class) {
-            supportFragmentManager.beginTransaction()
-                .setCustomAnimations(
-                    R.animator.slide_in_up,
-                    R.animator.slide_out_down,
-                    R.animator.slide_in_up,
-                    R.animator.slide_out_down
-                )
-                .replace(R.id.fragment_container, fragment)
-                .addToBackStack(null)
-                .commit()
-        } else{
-            supportFragmentManager.popBackStack()
-        }
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.animator.slide_in_up,
+                R.animator.slide_out_down,
+                R.animator.slide_in_up,
+                R.animator.slide_out_down
+            )
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
