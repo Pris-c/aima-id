f4fa4f71bebcc68c81c224fbbc1d4aa278dede10
pris-c <priscila.campoos@yahoo.com.br>
2024-11-02 15:35:25 +0000

    create class CalendarViewModel

---
+++ app/src/main/java/com/example/aima_id_app/ui/viewmodel/CalendarViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.aima_id_app.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.aima_id_app.data.model.db_model.AimaUnit
+
+class CalendarViewModel: ViewModel() {
+
+
+
+}
