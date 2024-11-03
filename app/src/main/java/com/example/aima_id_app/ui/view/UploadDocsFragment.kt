27b3adc7db77401ad22c11771957a8c35895bc3e
Gabriel Beli <gabrielimabelo@gmail.com>
2024-11-03 16:57:56 +0000

    uploadlayout finished

--- app/src/main/java/com/example/aima_id_app/ui/view/UploadDocsFragment.kt
+++ app/src/main/java/com/example/aima_id_app/ui/view/UploadDocsFragment.kt
@@ -1,62 +1,100 @@
 package com.example.aima_id_app.ui.view
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.aima_id_app.R
-import com.example.aima_id_app.data.repository.ServiceRepository
-import com.example.aima_id_app.ui.adapter.FileInputAdapter
 import com.example.aima_id_app.util.enums.DocType
+import com.google.android.material.textfield.TextInputEditText
+import android.provider.OpenableColumns
+
 
 class UploadDocsFragment : Fragment() {
 
-    private lateinit var recyclerViewFiles: RecyclerView
     private lateinit var spinner: Spinner
-    private val serviceRepository = ServiceRepository()
-    private var selectedFiles: MutableMap<DocType, Uri?> = mutableMapOf()
-
+    private lateinit var inputFileEditText: TextInputEditText
+    private var selectedFileUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_upload_docs, container, false)
-        recyclerViewFiles = view.findViewById(R.id.recyclerViewDocs)
-        recyclerViewFiles.layoutManager = LinearLayoutManager(context)
-        spinner = view.findViewById(R.id.nameDocs_input)
+        spinner = view.findViewById(R.id.nameStaff_input)
+        inputFileEditText = view.findViewById(R.id.inputFileEditText)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        serviceRepository.getAll { services ->
-            val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, services.map { it.name })
-            spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = spinnerAdapter
+        val docTypes = DocType.values().toList()
+        val spinnerAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, docTypes.map { getTitleForDocType(it) })
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = spinnerAdapter
 
-            /*spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    val selectedService = services[position]
-                    val fileInputs = selectedService.requiredDocuments
-                    val adapter = FileInputAdapter(requireContext(), fileInputs) { docType, uri ->
-                        selectedFiles[docType] = uri
-                    }
-                    recyclerViewFiles.adapter = adapter
-                }
 
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                    // ...
-                }
-            }*/
+        inputFileEditText.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                type = "*/*" // all types files
+            }
+            startActivityForResult(intent, REQUEST_CODE_PICK_FILE)
         }
+
+    }
+
+    private fun getTitleForDocType(docType: DocType): String {
+        return when (docType) {
+            DocType.PASSPORT -> "Passaporte"
+            DocType.VISA -> "Visto"
+            DocType.NIF -> "NIF"
+            DocType.NISS -> "NISS"
+            DocType.PROOF_INCOME -> "Prova de renda"
+            DocType.CITIZENSHIP_ID -> "Certidão nacionalidade"
+            DocType.ADDRESS_PROOF -> "Atestado de morada"
+            DocType.HEALTH_INSURANCE -> "Seguro Saúde"
+            DocType.CRIMINAL_RECORD -> "Atestado criminalidade"
+            DocType.EMPLOYMENT_CONTRACT -> "Contrato de trabalho"
+            DocType.SCHOOL_REGISTRATION -> "Matrícula escolar"
+            DocType.RESIDENCE_VISA -> "Visto de residência"
+            DocType.RESIDENT_PERMIT -> "Autorização de residência"
+            else -> "Selecione um documento/arquivo" // default
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_PICK_FILE && resultCode == Activity.RESULT_OK) {
+            val uri = data?.data
+            uri?.let {
+                inputFileEditText.setText(getFileNameFromUri(it))
+                selectedFileUri = it
+            }
+        }
+    }
+
+    private fun getFileNameFromUri(uri: Uri): String {
+        var fileName = ""
+        val cursor = requireContext().contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val displayNameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (displayNameIndex != -1) {
+                    fileName = it.getString(displayNameIndex)
+                }
+            }
+        }
+        return fileName
+    }
+
+    companion object {
+        const val REQUEST_CODE_PICK_FILE = 1001
     }
 }
