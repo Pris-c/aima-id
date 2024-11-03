de9d27a0c36f805ef27e61c0433cbea59106c28e
Priscila <99100607+Pris-c@users.noreply.github.com>
2024-11-03 12:59:03 +0000

    Aimaid 64 appointments scheduling components (#36)

--- app/src/main/java/com/example/aima_id_app/util/enums/DocType.kt
+++ app/src/main/java/com/example/aima_id_app/util/enums/DocType.kt
@@ -5,7 +5,7 @@ package com.example.aima_id_app.util.enums
  * Enum class representing different types of documents.
  * These document types can be used to categorize or identify various documents a user may possess.
  */
-enum class DocType(doc: String) {
+enum class DocType(val doc: String) {
     PASSPORT("Passaporte"),
     VISA("Visto"),
     RESIDENT_PERMIT("Autorização de Residência"),
---
+++ app/src/main/java/com/example/aima_id_app/util/enums/PossibleScheduling.kt
@@ -0,0 +1,24 @@
+package com.example.aima_id_app.util.enums
+
+import java.time.LocalTime
+
+
+enum class PossibleScheduling(val time: String) {
+    TIME_09_00("09:00"),
+    TIME_09_30("09:30"),
+    TIME_10_00("10:00"),
+    TIME_10_30("10:30"),
+    TIME_11_00("11:00"),
+    TIME_11_30("11:30"),
+    TIME_12_00("12:00"),
+    TIME_12_30("12:30"),
+    TIME_13_00("13:00"),
+    TIME_13_30("13:30"),
+    TIME_14_00("14:00"),
+    TIME_14_30("14:30"),
+    TIME_15_00("15:00"),
+    TIME_15_30("15:30"),
+    TIME_16_00("16:00"),
+    TIME_16_30("16:30"),
+    TIME_17_00("17:00");
+}
