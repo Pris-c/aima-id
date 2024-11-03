c683106c1e691368e62cf69b03d75959ef0745a1
pris-c <priscila.campoos@yahoo.com.br>
2024-11-03 13:39:33 +0000

    document CalendarViewModel

--- app/src/main/java/com/example/aima_id_app/ui/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/example/aima_id_app/ui/viewmodel/CalendarViewModel.kt
@@ -11,6 +11,22 @@ import com.example.aima_id_app.util.enums.PossibleScheduling
 import java.time.LocalDate
 import java.time.YearMonth
 
+/**
+ * ViewModel for managing the calendar-related functionalities of the application.
+ *
+ * This class handles the logic for loading available cities, checking the availability
+ * of days for appointments, and determining the availability of specific Aima Units
+ * on given days. It interacts with the `AppointmentRepository` and `AimaUnitRepository`
+ * to fetch necessary data and to perform CRUD operations on appointments and Aima Units.
+ *
+ * The `CalendarViewModel` exposes a LiveData list of available cities and provides
+ * methods to retrieve unavailable days, check day availability, and manage appointment
+ * scheduling based on unit capacities.
+ *
+ * @property appointmentRepository The repository for managing appointments.
+ * @property aimaUnitRepository The repository for managing Aima Units.
+ * @constructor Creates an instance of CalendarViewModel with the provided repositories.
+ */
 class CalendarViewModel(
 
     private val TAG: String = "DEBUG",
@@ -26,6 +42,10 @@ class CalendarViewModel(
     private val _cities = MutableLiveData<List<String>>()
     val cities: MutableLiveData<List<String>> = _cities
 
+    /**
+     * Loads available cities from the AimaUnit repository and updates the LiveData.
+     * Logs a message if no cities are found.
+     */
     private fun loadAvailableCities(){
         aimaUnitRepository.findAllAimaUnits { units ->
             if (units.isNotEmpty()){
@@ -38,6 +58,13 @@ class CalendarViewModel(
         }
     }
 
+    /**
+     * Retrieves the unavailable days for scheduling in a specified city and month.
+     *
+     * @param city The city to check availability.
+     * @param month The month to check for unavailable days.
+     * @param onComplete Callback with the list of unavailable days.
+     */
     fun getUnavailableDays(city: String, month: YearMonth, onComplete: (List<Int>) -> Unit) {
         val unavailableDays = mutableListOf<Int>()
         val daysInMonth = month.lengthOfMonth()
@@ -59,6 +86,14 @@ class CalendarViewModel(
         }
     }
 
+    /**
+     * Checks if a specific day is available to scheduling
+     * in a city by looking at its Aima Units.
+     *
+     * @param city The city to check.
+     * @param day The specific day to check for availability.
+     * @param callback Callback that returns the availability status.
+     */
     private fun isDayAvailable(city: String, day: LocalDate, callback: (Boolean) -> Unit) {
         getAimaUnitsByCity(city) { units ->
             if (units.isNotEmpty()) {
@@ -92,6 +127,13 @@ class CalendarViewModel(
         }
     }
 
+    /**
+     * Checks if a specific Aima Unit is available for scheduling on a given day.
+     *
+     * @param aimaUnitId The ID of the Aima Unit to check.
+     * @param day The specific day to check.
+     * @param onComplete Callback with the availability status.
+     */
     private fun isUnitAvailable(aimaUnitId: String, day: LocalDate, onComplete:
         (Boolean) -> Unit) {
 
@@ -112,6 +154,13 @@ class CalendarViewModel(
             }
     }
 
+    /**
+     * Retrieves all appointments for a specific Aima Unit on a given day.
+     *
+     * @param aimaUnitId The ID of the Aima Unit.
+     * @param day The specific day to check.
+     * @param onComplete Callback with the list of appointments.
+     */
     private fun getAppointmentsByUnit(aimaUnitId: String, day: LocalDate, onComplete:
         (List<Appointment>) -> Unit) {
             appointmentRepository.filterAppointmentsByUnit(aimaUnitId) { unitAppointments ->
@@ -125,6 +174,12 @@ class CalendarViewModel(
             }
     }
 
+    /**
+     * Retrieves Aima Units by city from the repository.
+     *
+     * @param city The city to filter Aima Units.
+     * @param onComplete Callback with the map of Aima Units.
+     */
     private fun getAimaUnitsByCity(city: String, onComplete: (Map<String, AimaUnit>) -> Unit) {
         aimaUnitRepository.getAimaUnitsByCity(city) { aimaUnits ->
             onComplete(aimaUnits)
