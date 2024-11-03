f0568adadd3fc1b6ba311f41b41194af105bf74c
Thais Freire <thaisfrsoares@gmail.com>
2024-11-03 14:36:05 +0000

    update

--- app/src/main/java/com/example/aima_id_app/ui/viewmodel/SubmittedDocsListViewModel.kt
+++ app/src/main/java/com/example/aima_id_app/ui/viewmodel/SubmittedDocsListViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.aima_id_app.ui.viewmodel
 
-class SubmittedDocsListViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.aima_id_app.data.model.db_model.UserDocument
+import com.example.aima_id_app.data.repository.UserDocumentRepository
+
+class SubmittedDocsListViewModel (
+    private val userDocumentRepository: UserDocumentRepository = UserDocumentRepository()
+
+) : ViewModel() {
+
+    fun getDocsToAnalyse(onComplete: (MutableList<UserDocument>) -> Unit) {
+
+        userDocumentRepository.filterSubmittedDocs { documents ->
+           onComplete(documents)
+        }
+    }
+
 }
