539033c581b8445627ea96b4d5611cef1a05463c
ArthurSSR-alt <ramalhoarthur09@gmail.com>
2024-11-04 17:56:31 +0000

    Tested and Reviewd

--- app/src/main/java/com/example/aima_id_app/MainActivity.kt
+++ app/src/main/java/com/example/aima_id_app/MainActivity.kt
@@ -25,8 +25,6 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    // Initialize the ViewModel using the viewModels delegate
-    private val statusViewModel: StatusViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,33 +38,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Observe the processes LiveData to log or display the data when it updates
-        statusViewModel.processes.observe(this) { processes ->
-            processes?.let {
-                Log.d("MainActivity", "Processes retrieved: $it")
-                Toast.makeText(this, "Processes Loaded: ${it.size}", Toast.LENGTH_SHORT).show()
-            } ?: run {
-                Log.d("MainActivity", "No processes found for this user.")
-                Toast.makeText(this, "No processes found for this user.", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // Call the function to load processes by user ID
-        statusViewModel.getProcessesByUserId()// Observe the processes LiveData to log or display the data when it updates
-        statusViewModel.processes.observe(this) { processes ->
-            processes?.let {
-                Log.d("Aoba", "Processes retrieved: $it")
-                Toast.makeText(this, "Processes Loaded: ${it.size}", Toast.LENGTH_SHORT).show()
-            } ?: run {
-                Log.d("Aoba", "No processes found for this user.")
-                Toast.makeText(this, "No processes found for this user.", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // Call the function to load processes by user ID
-        statusViewModel.getProcessesByUserId()
-        // Create a Handler to start the LoginActivity after 1 second
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
