bb19b75c5b48447712b65bc9aea83b9fb50df957
pris-c <priscila.campoos@yahoo.com.br>
2024-11-04 23:03:35 +0000

    implement UploadDocsViewModel

--- app/src/main/java/com/example/aima_id_app/ui/viewmodel/UploadDocsViewModel.kt
+++ app/src/main/java/com/example/aima_id_app/ui/viewmodel/UploadDocsViewModel.kt
@@ -4,10 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.aima_id_app.data.model.db_model.ServiceUser
-import com.example.aima_id_app.data.model.db_model.User
 import com.example.aima_id_app.data.model.db_model.UserDocument
-import com.example.aima_id_app.data.model.submodel.DocUser
 import com.example.aima_id_app.data.repository.DocStorageRepository
 import com.example.aima_id_app.data.repository.UserDocumentRepository
 import com.example.aima_id_app.data.repository.UserRepository
@@ -15,7 +12,6 @@ import com.example.aima_id_app.util.enums.DocStatus
 import com.example.aima_id_app.util.enums.DocType
 import com.example.aima_id_app.util.enums.UserRole
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import java.io.File
 import java.time.LocalDate
 
@@ -35,106 +31,136 @@ class UploadDocsViewModel(
 
     /**
      * Saves a new document or updates an existing document for the authenticated user.
-     * If the user already has a document of the same type, it updates the existing document.
-     * Otherwise, it saves a new document.
      *
-     * @param document The file to be saved.
-     * @param docType The type of document to be checked and saved.
-     * @return Returns the authenticated user or null if no user is authenticated.
+     * If the user has an existing document of the specified type, it updates that document.
+     * If not, it saves a new document.
+     *
+     * @param docFile The file to be saved or updated.
+     * @param docType The type of document to check for existence and save.
+     * @param onComplete A callback function that returns true if the operation was successful,
+     *                   or false if it failed.
      */
-    fun saveOrUpdateDoc(document: File, docType: DocType): User? {
-        val userId = auth.currentUser?.uid.toString()
+    fun saveOrUpdateDoc(docFile: File, docType: DocType, onComplete: (Boolean) -> Unit) {
+        val userId = auth.currentUser?.uid
+
+        if (userId == null) {
+            _documentErrorMessage.postValue("User is not authenticated.")
+            onComplete(false)
+            return
+        }
 
         userRepository.findUserById(userId) { user ->
-            if (user != null && (user.role).equals(UserRole.SERVICE_USER)) {
+            if (user != null && (user.role == UserRole.SERVICE_USER.role)) {
 
-                userDocumentRepository.findDocTypeByUser(userId, docType) { id, userDocuments ->
-                    if (userDocuments != null) {
-                        updateDocument(userDocuments,document)
-                    }else{
-                        newDocUpdate(document, docType)
+                userDocumentRepository.findDocTypeByUser(userId, docType) { oldDocId, oldDocument ->
+                    if (oldDocument != null && oldDocId != null) {
+                        updateDocument(oldDocId, oldDocument, docFile){ updated ->
+                            if (updated){
+                                onComplete(true)
+                            } else {
+                                onComplete(false)
+                            }
+                        }
+
+                    } else {
+                        saveNewDocument(userId, docFile, docType) { saved ->
+                            if (saved){
+                                onComplete(true)
+                            } else {
+                                onComplete(false)
+                            }
+                        }
                     }
                 }
-            }else{
+            } else {
                 _documentErrorMessage.postValue("Usuário inválido")
+                onComplete(false)
             }
         }
 
     }
 
     /**
-     * Saves a new document to Firebase Storage and creates an entry for it in the user's document repository.
-     * Updates the user's document list with the new document if the user is a ServiceUser.
+     * Saves a new document to Firebase Storage and creates an entry for it in the user's
+     * document collection.
      *
-     * @param document The file to be saved.
+     * @param userId The ID of the user who owns the document.
+     * @param file The file to be saved.
      * @param docType The type of the document to be saved.
+     * @param onComplete A callback function that indicates whether the save operation was successful.
+     *                   Returns true if successful, false otherwise.
      */
-    private fun newDocUpdate(document: File, docType: DocType) {
-        val userId = auth.currentUser?.uid ?: return
-        val serviceUser = auth.currentUser as ServiceUser
-
-        storageRepository.saveFile(document) { docPath ->
+    private fun saveNewDocument(userId: String, file: File, docType: DocType, onComplete: (Boolean) -> Unit) {
+        Log.d("FILE", "Saving..")
+        storageRepository.saveFile(file) { docPath ->
             if (docPath != null) {
                 val userDocument = UserDocument(
                     docPath = docPath,
-                    docType = docType.toString(),
+                    docType = docType.doc,
                     userId = userId,
                     submittedAt = LocalDate.now().toString()
                 )
 
-                userDocumentRepository.save(doc = userDocument) { docId ->
+                userDocumentRepository.save(userDocument) { docId ->
                     if (docId != null) {
-                        val newDoc = DocUser(docType, docId)
-                        serviceUser.docs.add(newDoc)
+                      onComplete(true)
                     } else {
-                        _documentErrorMessage.postValue("Failed to save UserDocument.")
+                        _documentErrorMessage.postValue("Falha ao salvar documento.")
                     }
                 }
+
             } else {
-                _documentErrorMessage.postValue("Failed to save file to storage.")
+                _documentErrorMessage.postValue("Falha ao salvar arquivo.")
+                onComplete(false)
             }
         }
     }
-    /**
-     * Updates an existing document by saving the new file to Firebase Storage, updating the document fields,
-     * and deleting the old file from storage.
-     *
-     * @param document The existing UserDocument to update.
-     * @param newFile The new file to upload.
-     */
-    fun updateDocument(document: UserDocument, newFile: File) {
-        val oldDocPath = document.docPath
 
-        storageRepository.saveFile(newFile) { newDocPath ->
-            if (newDocPath != null) {
+    /**
+     * Updates an existing document by saving a new file to Firebase Storage, updating the document
+     * fields, and deleting the old file from storage.
+     *
+     * This function attempts to save the provided new file to storage. If successful, it updates the
+     * existing UserDocument with the new file path, submission date, and status.
+     * After updating the document in the repository, it deletes the old file from storage.
+     *
+     * @param id The ID of the document to be updated.
+     * @param document The existing UserDocument to update.
+     * @param newFile The new file to upload and associate with the document.
+     * @param onComplete A callback function that returns true if the update was successful,
+     *                   or false if there was an error during the update operation.
+     */
+    private fun updateDocument(id: String, document: UserDocument, newFile: File, onComplete: (Boolean) -> Unit) {
+
+        val oldFilePath = document.docPath
+
+        storageRepository.saveFile(newFile) { newFilePath ->
+            Log.d("FILE", "Updating..")
+            if (newFilePath != null) {
                 document.apply {
-                    docPath = newDocPath
+                    docPath = newFilePath
                     submittedAt = LocalDate.now().toString()
                     status = DocStatus.SUBMITTED.toString()
-                    expirationDate = LocalDate.of(2125, 12, 30).toString()
                 }
-                userDocumentRepository.getDocumentId(document.userId, document.docType) { docId ->
-                    if (docId != null) {
-                        userDocumentRepository.update(docId, document) { updateSuccess ->
-                            if (updateSuccess!!) {
-                                storageRepository.deleteFile(oldDocPath) { deleteSuccess ->
-                                    if (deleteSuccess) {
-                                        _documentErrorMessage.postValue("Documento atualizado e arquivo antigo deletado com sucesso.")
-                                    } else {
-                                        _documentErrorMessage.postValue("Falha ao deletar o arquivo antigo.")
-                                    }
-                                }
-                            } else {
-                                _documentErrorMessage.postValue("Falha ao atualizar o arquivo.")
+
+                userDocumentRepository.update(id, document) { success ->
+                    if (success){
+                        storageRepository.deleteFile(oldFilePath){ deleted ->
+                            if (!deleted){
+                                _documentErrorMessage.postValue("Falha ao deletar arquivo antigo.")
                             }
                         }
+                        onComplete(true)
                     } else {
-                        _documentErrorMessage.postValue("ID do documento não encontrado.")
+                        _documentErrorMessage.postValue("Falha ao atualizar documento na base de dados.")
+                        onComplete(false)
                     }
                 }
             } else {
-                _documentErrorMessage.postValue("Falha ao salvar o novo arquivo.")
+                _documentErrorMessage.postValue("Falha ao atualizar arquivo.")
+                onComplete(false)
             }
         }
     }
+
 }
