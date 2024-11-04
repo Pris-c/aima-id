ec82ebf13a248127c07479465734b489a2e4346f
pris-c <priscila.campoos@yahoo.com.br>
2024-11-04 20:04:17 +0000

    implement UserDocsViewModel

--- app/src/main/java/com/example/aima_id_app/ui/viewmodel/UserDocsViewModel.kt
+++ app/src/main/java/com/example/aima_id_app/ui/viewmodel/UserDocsViewModel.kt
@@ -1,84 +1,42 @@
 package com.example.aima_id_app.ui.viewmodel
 
-import android.widget.Toast
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.aima_id_app.data.model.db_model.UserDocument
 import com.example.aima_id_app.data.repository.DocStorageRepository
 import com.example.aima_id_app.data.repository.UserDocumentRepository
-import com.example.aima_id_app.util.enums.DocStatus
-import java.io.File
-import java.time.LocalDate
+import com.google.firebase.auth.FirebaseAuth
 
 
 /**
- * ViewModel para gerenciar as interações com documentos do usuário.
- * Permite a recuperação e atualização de documentos associados a um usuário.
+ * ViewModel for managing user document interactions.
+ * Facilitates the retrieval and updating of documents associated with a user.
  *
- * @property storageRepository O repositório responsável por operações de armazenamento.
- * @property userDocumentRepository O repositório responsável por interações com documentos do usuário.
+ * @property userDocumentRepository The repository responsible for user document interactions.
+ * @property storageRepository The repository responsible for storage operations.
+ * @property auth The Firebase Authentication instance for managing user sessions.
  */
 class UserDocsViewModel(
     private val userDocumentRepository: UserDocumentRepository = UserDocumentRepository(),
-    private val storageRepository: DocStorageRepository = DocStorageRepository()
+    private val storageRepository: DocStorageRepository = DocStorageRepository(),
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 ) : ViewModel() {
 
-    private val _documentErrorMessage = MutableLiveData<String?>()
-    val documentErrorMessage: LiveData<String?> get() = _documentErrorMessage
-
-
     /**
-     * Retrieves a mutable list of UserDocument entries associated with the specified user ID.
-     * @param userId The ID of the user whose documents need to be retrieved.
-     * @return MutableList of UserDocument entries.
-     */
-    fun getUserDocs(userId: String, onComplete: (MutableList<UserDocument>) -> Unit) {
-        userDocumentRepository.filterByUser(userId) { userDocuments ->
-            onComplete(userDocuments.ifEmpty { mutableListOf() })
-        }
-    }
-
-    /**
-     * Updates an existing document by saving the new file to Firebase Storage, updating the document fields,
-     * and deleting the old file from storage.
+     * Retrieves documents associated with the currently authenticated user.
      *
-     * @param document The existing UserDocument to update.
-     * @param newFile The new file to upload.
+     * @param onComplete A callback function that receives a map of user documents once the
+     * retrieval is complete.
+     *
+     *    The key is a String identifier for the document, and the value is the
+     *    correspondingUserDocument object.
+     *
      */
-    fun updateDocument(document: UserDocument, newFile: File) {
-        val oldDocPath = document.docPath
-
-        storageRepository.saveFile(newFile) { newDocPath ->
-            if (newDocPath != null) {
-                document.apply {
-                    docPath = newDocPath
-                    submittedAt = LocalDate.now().toString()
-                    status = DocStatus.SUBMITTED
-                    expirationDate = LocalDate.of(2125, 12, 30).toString()
-                }
-                userDocumentRepository.getDocumentId(document.userId, document.docType) { docId ->
-                    if (docId != null) {
-                        userDocumentRepository.update(docId, document) { updateSuccess ->
-                            if (updateSuccess!!) {
-                                storageRepository.deleteFile(oldDocPath) { deleteSuccess ->
-                                    if (deleteSuccess) {
-                                        _documentErrorMessage.postValue("Documento atualizado e arquivo antigo deletado com sucesso.")
-                                    } else {
-                                        _documentErrorMessage.postValue("Falha ao deletar o arquivo antigo.")
-                                    }
-                                }
-                            } else {
-                                _documentErrorMessage.postValue("Falha ao atualizar o arquivo.")
-                            }
-                        }
-                    } else {
-                        _documentErrorMessage.postValue("ID do documento não encontrado.")
-                    }
-                }
-            } else {
-                _documentErrorMessage.postValue("Falha ao salvar o novo arquivo.")
-            }
+    fun getUserDocs(onComplete: (MutableMap<String, UserDocument>) -> Unit) {
+        val userId = auth.currentUser?.uid?: return
+        userDocumentRepository.getDocumentsByUser(userId) { userDocuments ->
+            onComplete(userDocuments)
         }
     }
+
+
 }
