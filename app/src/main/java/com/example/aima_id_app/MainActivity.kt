6fe17f59e45ee15182f05ece310d00a8dec9d183
ArthurSSR-alt <ramalhoarthur09@gmail.com>
2024-11-04 17:18:42 +0000

    Modified main to not conflit

--- app/src/main/java/com/example/aima_id_app/MainActivity.kt
+++ app/src/main/java/com/example/aima_id_app/MainActivity.kt
@@ -6,14 +6,16 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.aima_id_app.databinding.ActivityMainBinding
+import com.example.aima_id_app.ui.view.AdminActivity
 import com.example.aima_id_app.ui.view.LoginActivity
-import com.example.aima_id_app.util.validators.DocValidator
-import java.io.File
-import java.io.FileOutputStream
+import com.example.aima_id_app.ui.view.UserActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,7 +36,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
         // Create a Handler to start the LoginActivity after 1 second
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
@@ -45,10 +46,5 @@ class MainActivity : AppCompatActivity() {
 
             finish()
         }, 1000) // 1 second delay
-
-
-
-
     }
-
 }
