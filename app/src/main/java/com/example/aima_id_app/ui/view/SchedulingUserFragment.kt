8e86d2ecf93f8f768b85d3cf373e8c82359f3823
Gabriel Beli <gabrielimabelo@gmail.com>
2024-11-05 16:01:38 +0000

    ...

--- app/src/main/java/com/example/aima_id_app/ui/view/SchedulingUserFragment.kt
+++ app/src/main/java/com/example/aima_id_app/ui/view/SchedulingUserFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.aima_id_app.R
+import com.example.aima_id_app.ui.viewmodel.CalendarViewModel
+import java.time.YearMonth
 
 class SchedulingUserFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         return inflater.inflate(R.layout.fragment_user_scheduling, container, false)
     }
