d4a85f68fcdc3680654625ed82f9f5ffde2e6375
pris-c <priscila.campoos@yahoo.com.br>
2024-11-05 22:48:28 +0000

    Process submission working

--- app/src/main/java/com/example/aima_id_app/data/model/db_model/Service.kt
+++ app/src/main/java/com/example/aima_id_app/data/model/db_model/Service.kt
@@ -2,6 +2,7 @@ package com.example.aima_id_app.data.model.db_model
 
 class Service(
     val name: String = "",
+    val code: String = "",
     val hasPresencialAttendance: Boolean = false,
     val requiredDocuments: MutableList<String> = mutableListOf<String>()
 )
